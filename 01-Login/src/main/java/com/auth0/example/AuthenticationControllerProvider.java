b83665184413d1dbd7afb651787151a7170610d1
Josh Cain <jcain@redhat.com>
2018-12-08 12:51:02 -0600

    add support for local proxy to observe back-channel calls, switch to implicit flow with id_token only

--- 01-Login/src/main/java/com/auth0/example/AuthenticationControllerProvider.java
+++ 01-Login/src/main/java/com/auth0/example/AuthenticationControllerProvider.java
@@ -1,6 +1,7 @@
 package com.auth0.example;
 
 import com.auth0.AuthenticationController;
+import com.auth0.jwk.UrlJwkProvider;
 
 import javax.servlet.ServletConfig;
 import java.io.UnsupportedEncodingException;
@@ -13,6 +14,9 @@ public abstract class AuthenticationControllerProvider {
         String clientSecret = config.getServletContext().getInitParameter("com.auth0.clientSecret");
 
         return AuthenticationController.newBuilder(domain, clientId, clientSecret)
+                .withResponseType("id_token")
+                // https://jcain.auth0.com/.well-known/openid-configuration
+                .withJwkProvider(new UrlJwkProvider(String.format("https://%s/", domain)))
                 .build();
     }
 }
