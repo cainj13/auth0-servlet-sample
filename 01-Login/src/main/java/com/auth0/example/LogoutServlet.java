413011aefb0a36cf4b80604d0dedc645613c8bbd
Josh Cain <jcain@redhat.com>
2018-12-08 22:46:47 -0600

    add functional logout.  Previous sever invalidation was not effective, as a redirect loop to auth0 would simply log you back in

--- 01-Login/src/main/java/com/auth0/example/LogoutServlet.java
+++ 01-Login/src/main/java/com/auth0/example/LogoutServlet.java
@@ -1,5 +1,6 @@
 package com.auth0.example;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,12 +11,21 @@ import java.io.IOException;
 @WebServlet(urlPatterns = {"/logout"})
 public class LogoutServlet extends HttpServlet {
 
+    private String auth0Domain;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        // TODO static-ize the config params!
+        auth0Domain = config.getServletContext().getInitParameter("com.auth0.domain");
+    }
+
     @Override
     protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
         if (request.getSession() != null) {
             request.getSession().invalidate();
         }
-        response.sendRedirect("/login");
+        response.sendRedirect(String.format("https://%s/v2/logout?", auth0Domain));
     }
 
 }
